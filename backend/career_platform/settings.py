ebbf519952c5fbee79b0c3d12ea1d97ffa21c74c
amanbhattsquare <aman.bhattsquare@gmail.com>
2026-06-06 15:48:23 +0530

    add website app with initial configuration and models

--- backend/career_platform/settings.py
+++ backend/career_platform/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.website',
     'apps.resumes',
     'apps.accounts',
     'apps.admin_panel',
